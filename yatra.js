a9d1a3c40f37b406eb8142bf951755c425a1db9b
Soumyojyoti Saha <110711548+Soumyojyotisaha@users.noreply.github.com>
2023-12-19 13:17:11 +0530

    Update yatra.js

--- yatra.js
+++ yatra.js
@@ -1,24 +1,27 @@
+// Import necessary modules
 import playwright from 'playwright';
-import prompt from 'prompt-sync'; 
+import prompt from 'prompt-sync';
 
-const getInput = prompt(); 
-
-async function getYatraResult(origin, destin, trDate) {
+// Function to get Yatra search results
+const getYatraResult = async (origin, destin, trDate) => {
+  // Launch the Chromium browser
   const browser = await playwright.chromium.launch({
     headless: false,
   });
 
+  // Create a new page
   const page = await browser.newPage();
 
+  // Navigate to the Yatra website
   await page.goto('https://www.yatra.com/');
   await page.waitForTimeout(2000);
 
-  // Set origin
+  // Set origin location
   await page.getByRole('textbox', { name: 'Depart From' }).click();
   await page.getByText(`(${origin})`).click();
   await page.waitForTimeout(2000);
 
-  // Set destination
+  // Set destination location
   await page.getByRole('textbox', { name: 'Going To' }).click();
   await page.getByText(`(${destin})`).click();
   await page.waitForTimeout(2000);
@@ -28,12 +31,15 @@ async function getYatraResult(origin, destin, trDate) {
   await page.getByTitle(`${trDate}`).click();
   await page.waitForTimeout(2000);
 
+  await page.getByText('Non Stop Flights').click();
+  
   // Search for flights
   await page.getByRole('button', { name: 'Search Flights' }).click();
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(5000);
 
-  // Filter by price
+  // Filter search results by price
   await page.locator('span').filter({ hasText: 'Price' }).locator('span').click();
+  await page.waitForTimeout(2000);
 
   // Extract and log all text at the specified XPath
   const result = await page.$eval(
@@ -41,9 +47,12 @@ async function getYatraResult(origin, destin, trDate) {
     (element) => element.textContent.trim()
   );
 
+  // Close the browser
   await browser.close();
 
+  // Return the extracted result
   return result;
-}
+};
 
+// Export the function for external use
 export { getYatraResult };
