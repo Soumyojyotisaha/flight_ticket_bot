e191f24b14e60b7f6273e634308afbc8b474a2cf
Soumyojyoti Saha <110711548+Soumyojyotisaha@users.noreply.github.com>
2023-12-21 18:24:41 +0530

    Update agent.js

--- agent.js
+++ agent.js
@@ -1,23 +1,33 @@
+// Import required modules
 import { initializeAgentExecutorWithOptions } from "langchain/agents";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { SerpAPI } from "langchain/tools";
 import { Calculator } from "langchain/tools/calculator";
-import   dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import { getYatraResult } from './yatra.js';
 import { bookingResult } from './booking.js';
 import prompt from 'prompt-sync';
+
+// Load environment variables
 dotenv.config();
 
 // Function to get user input using prompt-sync
 const getInput = prompt();
 
-async function main() {
-    // Get user input for origin, destination, and departure date
-    const origin = getInput('Enter the origin airport code: ');
-    const destin = getInput('Enter the destination airport code: ');
-    const trDate = getInput('Enter the departure date (e.g.,Friday, 5 January): ');
+/**
+ * Main function to fetch Yatra results, interact with language models,
+ * and initiate the booking process.
+ * @param {string} a - Origin airport code 
+ * @param {string} b - Destination airport code 
+ * @param {string} c - Departure date 
+ */
+export async function agentResult(a=null, b=null, c=null) {
+  // Get user input for origin, destination, and departure date
+  const origin = a || getInput('Enter the origin airport code: ');
+  const destin = b || getInput('Enter the destination airport code: ');
+  const trDate = c || getInput('Enter the departure date (e.g., Friday, 5 January): ');
 
-  console.log("Please wait ⌛,while we fetch your preferred data 🔜.....");
+  console.log("Please wait ⌛, while we fetch your preferred data 🔜.....");
   // Fetch Yatra results for the given parameters
   const yatraResult = await getYatraResult(origin, destin, trDate);
 
@@ -36,7 +46,7 @@ async function main() {
   );
 
   // Run the language model to generate a request for the cheapest flight
-  const langChainResult = await executor.run(`Get me the cheapest flight from ${origin} to ${destin} on ${trDate} from Yatra Result: ${yatraResult}.The results should be shown row-wise as 1,2,3....Show only top 5 results.The results should be in the format: example: Air India AI-560: Departure - 06:55 from New Delhi, Arrival - 09:20 in Hyderabad, Duration - 2h 25m, Non-Stop, Fare - 5,227 `);
+  const langChainResult = await executor.run(`Get me the cheapest flight from ${origin} to ${destin} on ${trDate} from Yatra Result: ${yatraResult}. The results should be shown row-wise as 1,2,3.... Show only top 5 results. The results should be in the format: example: Air India AI-560: Departure - 06:55 from New Delhi, Arrival - 09:20 in Hyderabad, Duration - 2h 25m, Non-Stop, Fare - 5,227 `);
   console.log(langChainResult);
 
   // Extract and display flight options from the language model result
@@ -49,14 +59,19 @@ async function main() {
   const flightChosen = flightOptions[selectedOption - 1].flightCode;
   console.log(flightChosen);
 
-  const  divId = origin + destin + flightChosen.replace('-', '');
+  const divId = origin + destin + flightChosen.replace('-', '');
   console.log("Your selected Airline ✈️ is:", divId);
 
   // Initiate the booking process
-  const bookingProcessResult = await bookingResult(origin, destin, trDate, flightChosen,divId);
+  const bookingProcessResult = await bookingResult(origin, destin, trDate, flightChosen, divId);
   console.log(bookingProcessResult);
 }
-// Function to extract flight options from the language model result
+
+/**
+ * Extracts flight options from the language model result.
+ * @param {string} langChainResult - Result from the language model.
+ * @returns {Array} - Array of flight options.
+ */
 function extractFlightOptions(langChainResult) {
   const regex = /(\w{2}-\d{3,4}): Departure - \d{2}:\d{2} from .+Arrival - \d{2}:\d{2} in .+Duration - \d{1,2}h \d{1,2}m, Non-Stop, Fare - ([\d,]+)/g;
   const matches = [...langChainResult.matchAll(regex)];
@@ -68,7 +83,10 @@ function extractFlightOptions(langChainResult) {
   }));
 }
 
-// Function to display flight options
+/**
+ * Displays flight options.
+ * @param {Array} options - Array of flight options.
+ */
 function displayFlightOptions(options) {
   console.log('Flight Options:');
   options.forEach((opt) => {
@@ -77,4 +95,4 @@ function displayFlightOptions(options) {
 }
 
 // Execute the main function
-main();
+// agentResult();
